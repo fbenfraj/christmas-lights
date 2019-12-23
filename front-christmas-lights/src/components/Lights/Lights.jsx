699ee4aa884f1be2646ea18dc4f3f6cacb439509
fbenfraj <farouk.ben-fraj@sogeti.com>
2019-12-23 16:06:30 +0100

    [ADD] Project finished without bonuses

--- front-christmas-lights/src/components/Lights/Lights.jsx
+++ front-christmas-lights/src/components/Lights/Lights.jsx
@@ -5,6 +5,10 @@ import { Light } from "../Light/Light";
 export const Lights = props => {
   const [lights, setLights] = useState([
     {
+      id: 0,
+      isOn: false
+    },
+    {
       id: 1,
       isOn: false
     },
@@ -27,25 +31,46 @@ export const Lights = props => {
     {
       id: 6,
       isOn: false
-    },
-    {
-      id: 7,
-      isOn: false
     }
   ]);
-  const { isPlaying } = props;
+  const { isPlaying, intensity } = props;
   const [lightsInterval, setLightsInterval] = useState(null);
+  var actualLightId = 0;
 
   function handleLights() {
     setLights(
-      lights.map(light => (light.id === 1 ? { ...light, isOn: true } : light))
+      lights.map(light =>
+        light.id === actualLightId ? { ...light, isOn: true } : light
+      )
     );
+    actualLightId < 6 ? actualLightId++ : (actualLightId = 0);
   }
+
   useEffect(() => {
-    isPlaying
-      ? setLightsInterval(setInterval(handleLights, 1000))
-      : setLightsInterval(clearInterval(lightsInterval));
+    if (isPlaying) {
+      setLights(
+        lights.map(light =>
+          light.id === actualLightId ? { ...light, isOn: true } : light
+        )
+      );
+      setLightsInterval(
+        setInterval(handleLights, (10000 - 1000 * intensity) / 100)
+      );
+    } else {
+      setLights(lights.map(light => ({ ...light, isOn: false })));
+      setLightsInterval(clearInterval(lightsInterval));
+    }
   }, [isPlaying]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      setLightsInterval(clearInterval(lightsInterval));
+      setLights(lights.map(light => ({ ...light, isOn: false })));
+      setLightsInterval(
+        setInterval(handleLights, (10000 - 1000 * intensity) / 100)
+      );
+    }
+  }, [intensity]);
 
   return (
     <div className="lights">
--- front-christmas-lights/src/components/MainPage/MainPage.jsx
+++ front-christmas-lights/src/components/MainPage/MainPage.jsx
@@ -3,26 +3,17 @@ import { Lights } from "../Lights/Lights";
 import "./MainPage.scss";
 
 export const MainPage = () => {
-  const [isDisplayed, setIsDisplayed] = useState(true);
-  const [intensity, setIntensity] = useState(5);
+  const [isDisplayed, setIsDisplayed] = useState(false);
+  const [intensity, setIntensity] = useState(4);
+
   return (
     <>
       <main className="main">
-        <Lights isPlaying={isDisplayed} />
+        <section className="main-lights">
+          <h1 style={{ textAlign: "center", margin: 0 }}>Christmas Lights</h1>
+          <Lights isPlaying={isDisplayed} intensity={intensity} />
+        </section>
         <section className="main-settings">
-          <div className="main-settings-intensity">
-            <p>Intensity: {intensity}</p>
-            <input
-              type="range"
-              min="0"
-              max="10"
-              step="1"
-              value={intensity}
-              onChange={e => {
-                setIntensity(e.target.value);
-              }}
-            />
-          </div>
           <button
             onClick={() => {
               isDisplayed ? setIsDisplayed(false) : setIsDisplayed(true);
@@ -30,6 +21,21 @@ export const MainPage = () => {
           >
             {isDisplayed ? "Turn off" : "Turn on"}
           </button>
+          {!isDisplayed && (
+            <div className="main-settings-intensity">
+              <p>Intensity: {intensity}</p>
+              <input
+                type="range"
+                min="1"
+                max="7"
+                step="1"
+                value={intensity}
+                onChange={e => {
+                  setIntensity(e.target.value);
+                }}
+              />
+            </div>
+          )}
         </section>
       </main>
     </>
